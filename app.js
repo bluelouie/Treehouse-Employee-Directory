278e52b2b6dac426c92c2c75c0ede4e7a6819050
Jose L Medina <jmedinaluis92@yahoo.com>
2018-01-10 00:04:01 -0600

    Complete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// API request and callback--------------------------------------------------
+// -----------------------------------------------------------------
 
 var url = 'https://randomuser.me/api/?nat=us';
 
@@ -7,14 +9,14 @@ var dataOptions = {
 };
 
 var callBack = function (json, textStatus) {
-  var results = json.results;
+  window.results = json.results;
+  var results = window.results;
   var placeHTML = '<ul class="employeeList">';
 
-  $.each(results,function(index, el) {
-    var name = el.name.first.charAt(0).toUpperCase(0) + el.name.first.slice(1)
-    + " " + el.name.last.charAt(0).toUpperCase(0) + el.name.last.slice(1);
+  $.each(results, function(index, el) {
+    var name = el.name.first + " " + el.name.last;
     var email = el.email;
-    var city = el.location.city.charAt(0).toUpperCase(0) + el.location.city.slice(1);
+    var city = el.location.city;
     //---------------------------//
     placeHTML += '<li class="employee">';
     placeHTML += '<img src="' + el.picture.large +'" alt="Pic">';
@@ -23,49 +25,79 @@ var callBack = function (json, textStatus) {
   });
   placeHTML += '</ul>';
   $('.bodyDiv').html(placeHTML);
-}
 
+  //Call Modual() after callback
+  modal();
+}
 
 $.getJSON(url, dataOptions, callBack);
 
-$('.employeeList').magnificPopup({
-  delegate: 'li',
-  type:'image',
-  gallery: {
-    enabled: true, // set to true to enable gallery
 
-    preload: [0,2], // read about this option in next Lazy-loading section
 
-    navigateByImgClick: true,
+// Modal Scripting--------------------------------------------------
+// -----------------------------------------------------------------
+function modal() {
+  // Get the modal
+  var modal = document.getElementById('myModal');
 
-    arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+  // Get the button that opens the modal
+  var pictureSelect = $('.employee');
+  $.makeArray(pictureSelect);
 
-    tPrev: 'Previous (Left arrow key)', // title for left button
-    tNext: 'Next (Right arrow key)', // title for right button
-    tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-  },
-  image: {
-    markup: '<div class="mfp-figure">'+
-    '<div class="mfp-close"></div>'+
-    '<div class="mfp-img"></div>'+
-    '<div class="mfp-bottom-bar">'+
-    '<div class="mfp-title"></div>'+
-    '<div class="mfp-counter"></div>'+
-    '</div>'+
-    '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-    cursor: 'null', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+  // When the user clicks on the button, open the modal
+  $(pictureSelect).each(function(i, pictureSelect) {
+    $(pictureSelect).on( "click", modualBuilder);
+  });
 
-    titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
-    // Or the function that should return the title. For example:
-    // titleSrc: function(item) {
-    //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-    // }
-
-    verticalFit: true, // Fits image in area vertically
-
-    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
   }
 
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+//-------------------------------------------------------------------------------------
+/* Bulds the modal window based on click I tried not to use another API request */
+function modualBuilder(e) {
+  var users = window.results;
+  var target = e;
+  var modal = $('.modal')[0];
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].email === target.currentTarget.childNodes[1].firstElementChild.innerText) {
 
-});
+      var picture = '<img class="modal--pic" src="' + users[i].picture.large +'" alt="Pic">';
+      var name = '<h2>' + users[i].name.first + " " + users[i].name.last + '</h2>';
+      var email = "<h3>" + users[i].email + '</h3>';
+
+      var city = "<h3>" + users[i].location.city + '</h3>' + "<hr>";
+      var phoneNumber = "<h3>" + users[i].phone + '</h3>';
+      var address = "<h3>" + users[i].location.street + ', ' + users[i].location.state + ' ' + users[i].location.postcode + '</h3>';
+      var birthday = "<h3>Birthday: " + dobArrange(users[i].dob) + '</h3>';
+
+      var placeHTML = picture + name + email + city + phoneNumber + address + birthday;
+      $('.modal-content--section').html(placeHTML);
+    }
+  }
+  modal.style.display = 'block';
+}
+//-------------------------------------------------------------------------------------
+/* API .dob value was not formatted the way I needed so this rearranges it with slice()  */
+
+function dobArrange(dob) {
+  var year = dob.slice(0, 4);
+  var month = dob.slice(5, 7);
+  var day = dob.slice(8, 10);
+  var date = month + '/' + day + '/' + year;
+  return date;
+}
+
+
+//-------------------------------------------------------------------------------------
